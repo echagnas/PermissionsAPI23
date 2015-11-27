7baf898397a49bcf5ba9f5f2c83bc8f036e3bdbb
Emmanuel Chagnas <emmanuel.chagnas@viseo.com>
2015-11-27 11:54:41 +0100

    First commit

---
+++ app/src/main/java/manu/permissionsapi23/MainActivity.java
@@ -0,0 +1,79 @@
+package manu.permissionsapi23;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_READ_PHONE_STATE = 1;
+    private static final int PERMISSION_READ_PHONE_STATE_WITH_EXPLICATION = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        //Check permission
+        int permissionReadPhoneState = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+
+        switch (permissionReadPhoneState) {
+            case PackageManager.PERMISSION_DENIED:
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_READ_PHONE_STATE_WITH_EXPLICATION);
+                } else {
+                    Toast.makeText(this, R.string.permission_state_unknown, Toast.LENGTH_LONG).show();
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_READ_PHONE_STATE);
+                }
+                break;
+            case PackageManager.PERMISSION_GRANTED:
+                Toast.makeText(this, R.string.permission_granted, Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_READ_PHONE_STATE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, R.string.permission_granted, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_LONG).show();
+                }
+                break;
+            case PERMISSION_READ_PHONE_STATE_WITH_EXPLICATION:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, R.string.permission_granted, Toast.LENGTH_LONG).show();
+                } else {
+                    showMessageOKCancel(new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            checkPermission();
+                        }
+                    });
+                }
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void showMessageOKCancel(DialogInterface.OnClickListener backListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(R.string.permission_explication)
+                .setPositiveButton("OK", null)
+                .setNegativeButton("RETOUR", backListener)
+                .create()
+                .show();
+    }
+}
